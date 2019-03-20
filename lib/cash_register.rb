63fb7db7b8d2db10d7cc569dbd54ab680b91bbb1
Daniel Schneider <daschne8@gmail.com>
2019-03-20 16:54:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,6 +23,7 @@ class CashRegister
     title = @last_transaction[0]
     price = @last_transaction[1]
     quantity = @last_transaction[2]
+    quantiy.times {@items.remove(title)}
     @total -= price*quantity
   end
 
